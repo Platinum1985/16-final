56048bd5bb33fcf9509739bf12481b71cd05390f
Ishxan <ishmsu2@inbox.ru>
2026-04-09 02:15:49 +0300

    проверка gateway

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -68,7 +68,7 @@ public class BookingController {
     @PatchMapping("/{bookingId}")
     public ResponseEntity<Object> updateBookingApproval(
             @PathVariable int bookingId,
-            @RequestParam(value = "approved", defaultValue = "false") String approved,
+            @RequestParam(value = "approved") String approved,
             @RequestHeader("X-Sharer-User-Id") int itemOwnerId
     ) {
         log.info("Updating booking {} with approval status: {}", bookingId, approved);
