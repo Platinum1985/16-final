38b01cae33a474b382cd3f57b1468b025d2e6ce0
Ishxan <ishmsu2@inbox.ru>
2026-04-08 14:25:38 +0300

    проверка gateway

--- server/src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestMapper.java
@@ -1,11 +1,9 @@
 package ru.practicum.shareit.request;
 
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.user.User;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 public class ItemRequestMapper {
     public static ItemRequest toItemRequest(ItemRequestDto itemRequestDto, User requestor) {
