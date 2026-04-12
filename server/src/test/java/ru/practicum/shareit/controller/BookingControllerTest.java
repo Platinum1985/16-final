52a3919008a96f07b95eaed03aa1c68a5732769d
Ishxan <ishmsu2@inbox.ru>
2026-04-12 18:24:38 +0300

    тесты1

--- server/src/test/java/ru/practicum/shareit/controller/BookingControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/controller/BookingControllerTest.java
@@ -60,8 +60,8 @@ public class BookingControllerTest {
         LocalDateTime end = testBookingDto.getEnd();
 
         // Форматируем даты в строку, включая миллисекунды — это нужно для сравнения с ответом API
-        String startFormatted = start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS"));
-        String endFormatted = end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSS"));
+        String startFormatted = start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+        String endFormatted = end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
 
         when(bookingService.addBooking(any(BookingDto.class), eq(1)))
                 .thenReturn(testBooking);
